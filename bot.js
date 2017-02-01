cc158f3e0c638c7b3f0ae5dfcad91165e8777eca
Ron <ron@Rons-MacBook-Pro.local>
2017-02-01 20:21:18 +0700

    test

--- bot.js
+++ bot.js
@@ -4,6 +4,11 @@ const line = require('node-line-bot-api')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+
+var msgsJSON = require('./message.json');
+var msgs = JSON.parse(msgsJSON);
+
+console.log(msgs.eatword.1);
  
 // need raw buffer for signature validation
 app.use(bodyParser.json({
@@ -11,7 +16,9 @@ app.use(bodyParser.json({
     req.rawBody = buf
   }
 }))
- 
+
+app.use(bodyParser.json())
+
 // init with auth
 line.init({
   accessToken: 'tItdmtzc8IE/NBQ28ouLWWxVwuwXd8PSkjN9vFGhMjuNHOJU+tsRS+ZQn+xqqgo083mwevnDRcTPQYAGO2OevKA7lph3ddU4VXnMU8C5xFCeN5JKEG85C0k9UPm7AH657TtAKb+NCz9s3wdg1FpbHAdB04t89/1O/w1cDnyilFU=',
@@ -32,9 +39,11 @@ app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
         replyToken: event.replyToken,
         messages: [
           {
+
             type: 'text',
-            text: event.message.text
-          }
+            //text: event.message.text
+            test : 
+        }
         ]
       })
   })
@@ -45,5 +54,5 @@ app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
 
  
 app.listen(process.env.PORT || 80, () => {
-  console.log('Example app listening on port 80!')
+  console.log('Bot app listening on port 80!')
 })
