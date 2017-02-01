975bd63f27b865f341945658f17445a256138d24
Ron <ron@Rons-MacBook-Pro.local>
2017-02-01 18:44:20 +0700

    update

--- bot.js
+++ bot.js
@@ -1,2 +1,44 @@
+'use strict'
 
-console.log('Hello World');
+const line = require('node-line-bot-api')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+ 
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
+ 
+// init with auth
+line.init({
+  accessToken: 'tItdmtzc8IE/NBQ28ouLWWxVwuwXd8PSkjN9vFGhMjuNHOJU+tsRS+ZQn+xqqgo083mwevnDRcTPQYAGO2OevKA7lph3ddU4VXnMU8C5xFCeN5JKEG85C0k9UPm7AH657TtAKb+NCz9s3wdg1FpbHAdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: 'dd9e4f38db934a29efa6d268b33e3970'
+})
+ 
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
+ 
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
